aa71172ed3dcf373a8f9d9943a14cbb78573dd99
abdul-09 <abdul.9.hussein@gmail.com>
2024-10-14 20:19:01 +0300

    deployment

--- Backend/accounts/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class BulkEmailForm(forms.Form):
-    subject = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'size': '40'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}), required=True)
