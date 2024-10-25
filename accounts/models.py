ed400afa139ee40206e1aacfb3743226a905a200
abdul-09 <abdul.9.hussein@gmail.com>
2024-10-25 10:43:36 +0300

    updated backend

--- accounts/models.py
+++ accounts/models.py
@@ -54,7 +54,7 @@ class TrainingSchedule(models.Model):
         return self.title
 
 class Resource(models.Model):
-    schedule = models.ForeignKey(TrainingSchedule, on_delete=models.CASCADE, related_name='resources')
+    training_schedule = models.ForeignKey(TrainingSchedule, on_delete=models.CASCADE, related_name='resources')
     title = models.CharField(max_length=255)
     description = models.TextField()
     link = models.URLField()
