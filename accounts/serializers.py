b7b7fe341de8d706e8618b55ee9bfc16fc2c6054
abdul-09 <abdul.9.hussein@gmail.com>
2024-10-25 10:03:59 +0300

    updated backend

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -82,14 +82,10 @@ class ResourceSerializer(serializers.ModelSerializer):
 
 class TrainingScheduleSerializer(serializers.ModelSerializer):
     topic = serializers.SerializerMethodField()
-    
 
     class Meta:
         model = TrainingSchedule
         fields = ['id', 'topic', 'title', 'description', 'start_time']
-
-    def get_start_time(self, obj):
-        return obj.start_time
 
     def get_topic(self, obj):
         # Return the topic's name instead of the FK ID
