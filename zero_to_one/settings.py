f1bcde7a5d06cd8e2deab6814aa909d568013e1b
abdul-09 <abdul.9.hussein@gmail.com>
2024-11-12 15:01:52 +0300

    updated backend

--- zero_to_one/settings.py
+++ zero_to_one/settings.py
@@ -225,6 +225,7 @@ MEDIA_URL = 'media/'
 CORS_ALLOW_ALL_ORIGINS = True 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://klima360-frontend.vercel.app"
 ]
 CORS_ALLOW_METHODS = [
     'GET',
